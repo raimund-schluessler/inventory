2ec802ba83c44c3dc5b01e179c423a443c6c95f8
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2018-06-19 21:38:47 +0200

    Add item image

--- js/app/components/TheItemDetails.vue
+++ js/app/components/TheItemDetails.vue
@@ -31,136 +31,145 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 			</div>
 		</div>
 		<div id="itemdetails">
-			<span>{{ t('inventory', 'Properties') }}</span>
-			<table>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'Name') }}</span>
-					</td>
-					<td>
-						<span>{{ item.name }}</span>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'Maker') }}</span>
-					</td>
-					<td>
-						<span>{{ item.maker }}</span>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'Description') }}</span>
-					</td>
-					<td>
-						<span>{{ item.description }}</span>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'Item Number') }}</span>
-					</td>
-					<td>
-						<span>{{ item.itemNumber }}</span>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'Link') }}</span>
-					</td>
-					<td>
-						<span><a target="_blank" v-bind:href="item.link">{{ item.link }}</a></span>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'GTIN') }}</span>
-					</td>
-					<td>
-						<span>{{ item.gtin }}</span>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'Details') }}</span>
-					</td>
-					<td>
-						<span>{{ item.details }}</span>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'Comment') }}</span>
-					</td>
-					<td>
-						<span>{{ item.comment }}</span>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<span>{{ t('inventory', 'Categories') }}</span>
-					</td>
-					<td>
-						<ul class="categories">
-								<li v-for='category in item.categories' :key='category.id'>
-								<span>{{ category.name }}</span>
-							</li>
-						</ul>
-					</td>
-				</tr>
-			</table>
-			<br/>
-			<span>{{ t('inventory', 'Instances') }}</span>
-			<table>
-				<thead>
+			<div class="item_images">
+				<img src="/nextcloud/index.php/../apps/inventory/img/inventory.svg"/>
+			</div>
+			<div>
+				<span>{{ t('inventory', 'Properties') }}</span>
+				<table>
 					<tr>
-						<th>
-							<span>{{ t('inventory', 'Count') }}</span>
-						</th>
-						<th>
-							<span>{{ t('inventory', 'Available') }}</span>
-						</th>
-						<th>
-							<span>{{ t('inventory', 'Price') }}</span>
-						</th>
-						<th>
-							<span>{{ t('inventory', 'Date') }}</span>
-						</th>
-						<th>
-							<span>{{ t('inventory', 'Vendor') }}</span>
-						</th>
-						<th>
-							<span>{{ t('inventory', 'Place') }}</span>
-						</th>
-						<th>
+						<td>
+							<span>{{ t('inventory', 'Name') }}</span>
+						</td>
+						<td>
+							<span>{{ item.name }}</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span>{{ t('inventory', 'Maker') }}</span>
+						</td>
+						<td>
+							<span>{{ item.maker }}</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span>{{ t('inventory', 'Description') }}</span>
+						</td>
+						<td>
+							<span>{{ item.description }}</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span>{{ t('inventory', 'Item Number') }}</span>
+						</td>
+						<td>
+							<span>{{ item.itemNumber }}</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span>{{ t('inventory', 'Link') }}</span>
+						</td>
+						<td>
+							<span><a target="_blank" v-bind:href="item.link">{{ item.link }}</a></span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span>{{ t('inventory', 'GTIN') }}</span>
+						</td>
+						<td>
+							<span>{{ item.gtin }}</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<span>{{ t('inventory', 'Details') }}</span>
+						</td>
+						<td>
+							<span>{{ item.details }}</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
 							<span>{{ t('inventory', 'Comment') }}</span>
-						</th>
+						</td>
+						<td>
+							<span>{{ item.comment }}</span>
+						</td>
 					</tr>
-				</thead>
-				<tbody>
-					<tr class="handler"
-					v-for="instance in item.instances"
+					<tr>
+						<td>
+							<span>{{ t('inventory', 'Categories') }}</span>
+						</td>
+						<td>
+							<ul class="categories">
+								<li v-for='category in item.categories' :key='category.id'>
+									<span>{{ category.name }}</span>
+								</li>
+							</ul>
+						</td>
+					</tr>
+				</table>
+			</div>
+			<br/>
+			<div>
+				<span>{{ t('inventory', 'Instances') }}</span>
+				<table>
+					<thead>
+						<tr>
+							<th>
+								<span>{{ t('inventory', 'Count') }}</span>
+							</th>
+							<th>
+								<span>{{ t('inventory', 'Available') }}</span>
+							</th>
+							<th>
+								<span>{{ t('inventory', 'Price') }}</span>
+							</th>
+							<th>
+								<span>{{ t('inventory', 'Date') }}</span>
+							</th>
+							<th>
+								<span>{{ t('inventory', 'Vendor') }}</span>
+							</th>
+							<th>
+								<span>{{ t('inventory', 'Place') }}</span>
+							</th>
+							<th>
+								<span>{{ t('inventory', 'Comment') }}</span>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr class="handler"
+						v-for="instance in item.instances"
 						:key="instance.id">
-						<td>{{ instance.count }}</td>
-						<td>{{ instance.available }}</td>
-						<td>{{ instance.price }}</td>
-						<td>{{ instance.date }}</td>
-						<td>{{ instance.vendor }}</td>
-						<td>{{ getPlace(instance) }}</td>
-						<td>{{ instance.comment }}</td>
-					</tr>
-				</tbody>
-			</table>
+							<td>{{ instance.count }}</td>
+							<td>{{ instance.available }}</td>
+							<td>{{ instance.price }}</td>
+							<td>{{ instance.date }}</td>
+							<td>{{ instance.vendor }}</td>
+							<td>{{ getPlace(instance) }}</td>
+							<td>{{ instance.comment }}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 			<br/>
-			<span>{{ t('inventory', 'Subitems') }}</span>
-			<items-table v-bind:items="subItems"></items-table>
-			<br/>
-			<span>{{ t('inventory', 'Parent items') }}</span>
-			<items-table v-bind:items="parentItems"></items-table>
-			<br/>
-			<span>{{ t('inventory', 'Related items') }}</span>
-			<items-table v-bind:items="relatedItems"></items-table>
+			<div>
+				<span>{{ t('inventory', 'Subitems') }}</span>
+				<items-table v-bind:items="subItems"></items-table>
+				<br/>
+				<span>{{ t('inventory', 'Parent items') }}</span>
+				<items-table v-bind:items="parentItems"></items-table>
+				<br/>
+				<span>{{ t('inventory', 'Related items') }}</span>
+				<items-table v-bind:items="relatedItems"></items-table>
+			</div>
 		</div>
 	</div>
 </template>
