ae9115588e4096c2c3050473cc649106709253c9
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2018-06-22 22:41:35 +0200

    Lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,6 @@ if (process.env.NODE_ENV === 'production') {
 
 // test specific setups
 if (process.env.NODE_ENV === 'test') {
-	module.exports.externals = [require('webpack-node-externals')()]
-	module.exports.devtool = 'eval'
+	module.exports.externals = [require('webpack-node-externals')()];
+	module.exports.devtool = 'eval';
   }
