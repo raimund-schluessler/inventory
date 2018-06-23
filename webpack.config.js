a038f38573c1cc9ceaf37881eab79e4c5745c9c8
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2018-06-23 11:31:17 +0200

    Fix production build

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
 };
 
 if (process.env.NODE_ENV === 'production') {
-	module.exports.devtool = '#source-map';
+	module.exports.devtool = 'none';
 	module.exports.mode = "production";
 	// http://vue-loader.vuejs.org/en/workflow/production.html
 	module.exports.plugins = (module.exports.plugins || []).concat([
