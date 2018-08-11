5e9547b73cf5e7645f9b9429ba9ed6316b427b9a
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2018-08-11 11:25:00 +0200

    Fix item selection click handlers

--- js/app/components/ItemsTable.vue
+++ js/app/components/ItemsTable.vue
@@ -81,7 +81,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 			:key="item.id" v-bind:class="{ selected: isSelected(item) }" v-if="mode === 'navigation'">
 				<td class="selection">
 					<input v-bind:id="'select-item-' + item.id + '-' + _uid" class="selectCheckBox checkbox" type="checkbox"
-					 v-bind:value="item.id" v-model="selectedItemIDs">
+					 v-bind:value="item.id" :checked="isSelected(item)">
 					<label v-bind:for="'select-item-' + item.id + '-' + _uid" @click="selectItem(item)">
 						<span class="hidden-visually">{{ t('inventory', 'Select') }}</span>
 					</label>
@@ -109,8 +109,8 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 			:key="item.id" @click="selectItem(item)" v-bind:class="{ selected: isSelected(item) }" v-if="mode === 'selection'">
 				<td class="selection">
 					<input v-bind:id="'select-item-' + item.id + '-' + _uid" class="selectCheckBox checkbox" type="checkbox"
-					 v-bind:value="item.id" v-model="selectedItemIDs">
-					<label v-bind:for="'select-item-' + item.id + '-' + _uid" @click="selectItem(item)">
+					 v-bind:value="item.id" :checked="isSelected(item)">
+					<label v-bind:for="'select-item-' + item.id + '-' + _uid" @click.prevent>
 						<span class="hidden-visually">{{ t('inventory', 'Select') }}</span>
 					</label>
 				</td>
@@ -222,9 +222,6 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 				}
 			},
 			filteredItems() {
-				if (!this.items) {
-					return;
-				}
 				if (!this.searchString) {
 					return this.items;
 				}
