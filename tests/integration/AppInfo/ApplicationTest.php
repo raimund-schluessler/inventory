658338e914d223101610f3895e89bdddcccb55ed
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2019-09-23 13:21:28 +0200

    Add php unit and integration tests

---
+++ tests/integration/AppInfo/ApplicationTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Nextcloud - Inventory
+ *
+ * @author Raimund Schlüßler
+ * @copyright 2019 Raimund Schlüßler <raimund.schluessler@mailbox.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+use OCP\AppFramework\App;
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+
+	private $container;
+	private $app;
+
+	public function setUp(): void {
+		parent::setUp();
+		$this->app = new \OCA\Inventory\AppInfo\Application();
+		$this->container = $this->app->getContainer();
+	}
+
+	public function testAppInstalled() {
+		$appManager = $this->container->query('OCP\App\IAppManager');
+		$this->assertTrue($appManager->isInstalled('inventory'));
+	}
+
+	public function testNavigation() {
+		$navigationManager = \OC::$server->getNavigationManager();
+		$navigationManager->clear();
+		$enabled = array_key_exists('inventory', $navigationManager->getAll());
+		$this->assertTrue($enabled);
+	}
+
+}
---
+++ tests/unit/Controller/PageControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Nextcloud - Inventory
+ *
+ * @author Raimund Schlüßler
+ * @copyright 2019 Raimund Schlüßler <raimund.schluessler@mailbox.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Inventory\Controller;
+
+use OCP\AppFramework\Http\TemplateResponse;
+use PHPUnit\Framework\TestCase as Base;
+
+
+class PageControllerTest extends Base {
+
+	private $controller;
+
+	public function setUp(): void {
+		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+		$config = $this->getMockBuilder('OCP\IConfig')->getMock();
+
+		$this->controller = new PageController(
+			'inventory',
+			$request,
+			'admin',
+			$config,
+		);
+	}
+
+
+	public function testIndex() {
+		$result = $this->controller->index();
+
+		$this->assertEquals('main', $result->getTemplateName());
+		$this->assertEquals('user', $result->getRenderAs());
+		$this->assertTrue($result instanceof TemplateResponse);
+	}
+}
