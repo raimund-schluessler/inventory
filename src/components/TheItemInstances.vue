6aeb8f57e367e1c32f8e8163840ee90881177722
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2019-09-29 20:55:54 +0200

    Show placeholder on input elements

--- src/components/TheItemInstances.vue
+++ src/components/TheItemInstances.vue
@@ -72,6 +72,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 					<td v-for="instanceProperty in instanceProperties" :key="instanceProperty.key" :class="instanceProperty.width">
 						<input v-model="newInstance[instanceProperty.key]"
 							type="text"
+							:placeholder="instanceProperty.name"
 							:name="instanceProperty.key"
 							form="new_instance"
 						>
