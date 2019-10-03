29c22b68f83ebfca4b05cfcc7abcd6348db25cae
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2019-10-03 11:27:15 +0200

    Fix lint error for test setup

--- tests/setup.js
+++ tests/setup.js
@@ -1,12 +1,10 @@
 import 'regenerator-runtime/runtime'
 import VueTestUtils from '@vue/test-utils'
 
-global.expect = require('expect')
-
 // Mock nextcloud translate functions
-VueTestUtils.config.mocks["t"] = function(app, string) {
+VueTestUtils.config.mocks.t = function(app, string) {
 	return string
 }
-VueTestUtils.config.mocks["n"] = function(app, singular, plural, count) {
+VueTestUtils.config.mocks.n = function(app, singular, plural, count) {
 	return singular
 }
