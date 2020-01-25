303f7cb386bb1f33ebc630c59f8e1ab2ace0e577
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2020-01-25 21:14:54 +0100

    Switch to named imports for vue-test-utils

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,5 @@
 import 'regenerator-runtime/runtime'
-import VueTestUtils from '@vue/test-utils'
+import { config } from '@vue/test-utils'
 import { OC } from './OC.js'
 import OCA from './OCA.js'
 
@@ -7,9 +7,9 @@ global.OC = new OC()
 global.OCA = OCA
 
 // Mock nextcloud translate functions
-VueTestUtils.config.mocks.t = function(app, string) {
+config.mocks.t = function(app, string) {
 	return string
 }
-VueTestUtils.config.mocks.n = function(app, singular, plural, count) {
+config.mocks.n = function(app, singular, plural, count) {
 	return singular
 }
