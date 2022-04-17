edd0be61e29abb2c1f1d5c425b01b3042b7e24ee
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2022-04-17 10:56:43 +0200

    Run eslint on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
 const webpackConfig = require('@nextcloud/webpack-vue-config')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+webpackConfig.plugins.push(
+    new ESLintPlugin({
+        extensions: ['js', 'vue'],
+    }),
+)
 
 module.exports = webpackConfig
