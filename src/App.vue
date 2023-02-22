95dec53e098a8c51361b000a023ee6c1d1da95e2
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2023-02-22 12:54:28 +0100

    Don't use deprecated title prop

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 				<NcAppNavigationItem v-for="view in views"
 					:key="view.id"
 					:to="`/${view.id}/`"
-					:title="view.name">
+					:name="view.name">
 					<template #icon>
 						<component :is="view.icon"
 							:size="20" />
