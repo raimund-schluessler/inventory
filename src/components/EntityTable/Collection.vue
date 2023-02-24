502c276190d3696625f9c1ee7c28d79938d33918
Raimund Schlüßler <raimund.schluessler@mailbox.org>
2023-02-24 12:55:18 +0100

    Don't use tag prop on RouterLink

--- src/components/EntityTable/Collection.vue
+++ src/components/EntityTable/Collection.vue
@@ -37,7 +37,6 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
 		</div>
 		<div class="column">
 			<RouterLink :to="`/${collection}/${encodePath(entity.path)}`"
-				tag="a"
 				@click.ctrl.prevent>
 				<div class="thumbnail">
 					<div :style="{ backgroundImage: getThumbnailUrl }"
