0fe518d8f84d273dfba47494b10e03e1bd5f252e
intervisionlord <ads-net@mail.ru>
2024-02-11 17:14:34 +0300

    RU localization

---
+++ l10n/ru_RU.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "inventory",
+    {
+    "Inventory" : "Инвентарь",
+    "places" : "места",
+    "Places" : "Места",
+    "Items" : "Объекты",
+	"Description" : "Описание"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/ru_RU.php
@@ -0,0 +1,10 @@
+<?php
+
+$TRANSLATIONS = [
+	"Inventory" => "Инвентарь",
+	"places" => "места",
+	"Places" => "Места",
+	"Items" => "Объекты",
+	"Description" => "Описание"
+];
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
